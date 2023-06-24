8b73e20292cbaff92faa4ca0fd990d9f1499e5ea
Lukman Mahmud <mistamann49@gmail.com>
2023-06-24 13:42:48 +0100

    Add Generator steppers completed

--- client/src/components/addGenerator/AddGenerator.jsx
+++ client/src/components/addGenerator/AddGenerator.jsx
@@ -1,5 +1,8 @@
 import { useState } from 'react'
-import { Button, Container, Stack, Step, StepButton, Stepper } from '@mui/material'
+import { Box, Button, Container, Stack, Step, StepButton, Stepper } from '@mui/material'
+import AddLocation from './addLocation/AddLocation'
+import AddDetails from './addDetails/AddDetails'
+import AddImages from './addImages/AddImages'
 
 const AddGenerator = () => {
   const [activeStep, setActiveStep] = useState(0)
@@ -8,6 +11,26 @@ const AddGenerator = () => {
     { label: 'Details', completed: false },
     { label: 'Images', completed: false },
   ])
+
+  const handleNext = () => {
+    if (activeStep < steps.length - 1) {
+      setActiveStep((activeStep) => activeStep + 1);
+    } else {
+      const stepIndex = findUnfinished();
+      setActiveStep(stepIndex);
+    }
+  };
+  const checkDisabled = () => {
+    if (activeStep < steps.length - 1) return false;
+    const index = findUnfinished();
+    if (index !== -1) return false;
+    return true;
+  };
+
+  const findUnfinished = () => {
+    return steps.findIndex((step) => !step.completed);
+  };
+
   return (
     <Container sx={{ my: 4 }}>
       <Stepper
@@ -24,15 +47,29 @@ const AddGenerator = () => {
           </Step>
         ))}
       </Stepper>
+      <Box>
+        {{
+          0: <AddLocation />,
+          1: <AddDetails />,
+          2: <AddImages />,
+        }[activeStep]}
+      </Box>
       <Stack
         direction='row'
-        sx={{pt: 2, pb: 7, justifyContent: 'space-around'}}
+        sx={{ pt: 2, pb: 7, justifyContent: 'space-around' }}
       >
         <Button
           color='inherit'
-          disabled={}
+          disabled={!activeStep}
+          onClick={() => setActiveStep(activeStep => activeStep - 1)}
         >
           Back
+        </Button>
+        <Button
+          disabled={checkDisabled()}
+          onClick={handleNext}
+        >
+          Next
         </Button>
       </Stack>
     </Container>
